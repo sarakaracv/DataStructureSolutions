dce7d17926ab389b526a7343716586f129f30bc0
sarankara <gulcinsarankara@hotmail.com>
2023-06-08 19:41:01 -0400

    solutions

--- src/main/java/org/example/leetCodeTask/FlattenNestedListIterator341.java
+++ src/main/java/org/example/leetCodeTask/FlattenNestedListIterator341.java
@@ -26,62 +26,117 @@ public class FlattenNestedListIterator341 {
  * }
  */
 
-    public class NestedIterator implements Iterator<Integer> {
-        private Stack<NestedInteger> stack;
 
-        public NestedIterator(List<NestedInteger> nestedList) {
-            stack = new Stack<>();
-            pushListToStack(nestedList);
-        }
+// This is the interface provided by LeetCode for the NestedInteger class.
+// You don't need to implement it.
+interface NestedInteger {
+    boolean isInteger();
+    Integer getInteger();
+    List<NestedInteger> getList();
+}
 
-        @Override
-        public Integer next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return stack.pop().getInteger();
-        }
+class NestedIterator implements Iterator<Integer> {
+    private Deque<Iterator<NestedInteger>> stack;
 
-        @Override
-        public boolean hasNext() {
-            while (!stack.isEmpty() && !stack.peek().isInteger()) {
-                pushListToStack(stack.pop().getList());
-            }
-            return !stack.isEmpty();
-        }
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new ArrayDeque<>();
+        stack.push(nestedList.iterator());
+    }
 
-        private void pushListToStack(List<NestedInteger> nestedList) {
-            for (int i = nestedList.size() - 1; i >= 0; i--) {
-                stack.push(nestedList.get(i));
-            }
-        }
+    @Override
+    public Integer next() {
+        hasNext(); // Ensure the stack contains an iterator pointing to an integer
+        return stack.peek().next().getInteger();
+    }
 
-        //second
-
-
-        public class NestedIterator implements Iterator<Integer> {
-            private Deque<NestedInteger> stack;
-
-            public NestedIterator(List<NestedInteger> nestedList) {
-                stack = new ArrayDeque<>(nestedList);
-            }
-
-            @Override
-            public Integer next() {
-                return stack.removeFirst().getInteger();
-            }
-
-            @Override
-            public boolean hasNext() {
-                while (!stack.isEmpty() && !stack.peekFirst().isInteger()) {
-                    List<NestedInteger> nestedList = stack.removeFirst().getList();
-                    for (int i = nestedList.size() - 1; i >= 0; i--) {
-                        stack.addFirst(nestedList.get(i));
-                    }
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            if (!stack.peek().hasNext()) {
+                stack.pop();
+            } else {
+                NestedInteger nestedInteger = stack.peek().next();
+                if (nestedInteger.isInteger()) {
+                    stack.push(Collections.singleton(nestedInteger).iterator());
+                    return true;
                 }
-                return !stack.isEmpty() && stack.peekFirst().isInteger();
+                stack.push(nestedInteger.getList().iterator());
             }
+        }
+        return false;
+    }
+
+}
+// second solution
+
+ class NestedIterator1 implements Iterator<Integer> {
+    List<Integer> list=null;
+    int curr=0;
+    public NestedIterator1(List<NestedInteger> nestedList) {
+        list=new ArrayList<>();
+        for(NestedInteger i:nestedList){
+            helper(i);
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return list.get(curr++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return curr<list.size();
+    }
+    public void helper(NestedInteger value){
+        if(value.isInteger()) list.add(value.getInteger());
+        else {
+            for(NestedInteger i:value.getList())helper(i);
+        }
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+// third solution
+
+ class NestedIterator3 implements Iterator<Integer> {
+
+    List<Integer> result = new ArrayList<>();
+    int i =0;
+
+
+    public NestedIterator3(List<NestedInteger> nestedList) {
+        for(int i=0;i<nestedList.size();i++) {
+            if(nestedList.get(i).isInteger())
+                result.add(nestedList.get(i).getInteger());
+            else
+                helper(nestedList.get(i));
+        }
+    }
+
+    public void helper(NestedInteger data) {
+        if(data.isInteger())
+            result.add(data.getInteger());
+
+        else {
+            for(int i=0;i<data.getList().size();i++)
+                helper(data.getList().get(i));
         }
 
     }
 
+    @Override
+    public Integer next() {
+        return result.get(i++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (i<result.size());
+    }
+}
